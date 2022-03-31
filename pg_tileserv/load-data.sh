9d2bde72d046c9593a7031fb695d228969d6fdc5
Ben Best <ben@ecoquants.com>
2022-03-31 07:34:02 -0700

    pg_tileserv tweaks for working locally

--- pg_tileserv/load-data.sh
+++ pg_tileserv/load-data.sh
@@ -3,12 +3,12 @@
 #
 
 # Load Admin 0 countries
-docker-compose exec pg_tileserv_db sh -c "shp2pgsql -D -s 4326 /work/ne_50m_admin_0_countries.shp | psql -U tileserv -d tileserv"
+docker-compose exec pg_tileserv_db sh -c "shp2pgsql -D -s 4326 /work/ne_50m_admin_0_countries/ne_50m_admin_0_countries.shp | psql -U tileserv -d tileserv"
 
 # Load Vancouver Water Hydrants
-docker-compose exec pg_tileserv_db sh -c "shp2pgsql -D -s 26910 -I /work/water-hydrants.shp hydrants | psql -U tileserv -d tileserv"
+docker-compose exec pg_tileserv_db sh -c "shp2pgsql -D -s 26910 -I /work/water-hydrants/water-hydrants.shp hydrants | psql -U tileserv -d tileserv"
 
 # Load SQL Functions for OpenLayers example
 cp ../openlayers/openlayers-function-click.sql ./data/
 docker-compose exec pg_tileserv_db sh -c "cat /work/openlayers-function-click.sql | psql -U tileserv -d tileserv"
-rm ./data/openlayers-function-click.sql
+#rm ./data/openlayers-function-click.sql
